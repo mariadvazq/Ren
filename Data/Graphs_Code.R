16dce3b48c4c1d2ca71edd139cf12cde7bfcbb52
Maria D. Vazquez <43020186+mariadvazq@users.noreply.github.com>
2018-10-13 18:17:43 -0400

    Update Graphs_Code.R

--- Data/Graphs_Code.R
+++ Data/Graphs_Code.R
@@ -1,5 +1,4 @@
 stripchart(ren$No.Items, method= "stack", xlab = "Number of Items Purchased for Each Project")
-stripchart(ren$Days, method= "stack", xlab = "Number of Days spent on a Project")
-hist(ren$Days, main = "Days Spent on a Project", xlab = "Number of Days")
+hist(ren$Days, breaks = 6, main = "Days Spent on a Project", xlab = "Number of Days")
 hist(ren$Spent, main = "Amount Spent Per Project", xlab = "Amount", breaks = 9)
 

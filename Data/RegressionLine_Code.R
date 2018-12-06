83a54d0b71b15e1b601839ceb3230d45f3b88300
Maria D. Vazquez <43020186+mariadvazq@users.noreply.github.com>
2018-12-06 17:45:25 -0500

    Add files via upload
    Fixed regression code

--- Data/RegressionLine_Code.R
+++ Data/RegressionLine_Code.R
@@ -1,7 +1,8 @@
 data_minus1 <- ren
 withoutLargestOutlier <- data_minus1[-c(30),]
 withoutLargestOutlier
-withoutLargestOutlier.lm <- lm(Invoice~Total.Costs, data = withoutLargestOutlier)
+withoutLargestOutlier.lm <- lm(X.Gain~Total.Costs, data = withoutLargestOutlier)
 coef(withoutLargestOutlier.lm)
-plot(Invoice~Total.Costs, data = withoutLargestOutlier, main ="Ren's Profit")
+plot(X.Gain~Total.Costs, data = withoutLargestOutlier, main ="Ren's Profit")
 abline(coef(withoutLargestOutlier.lm))
+

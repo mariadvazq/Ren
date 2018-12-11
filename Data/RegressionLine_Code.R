2eaa2e660507bd76f5d06508d97e4bda6223f8ca
Maria D. Vazquez <43020186+mariadvazq@users.noreply.github.com>
2018-12-10 21:02:39 -0500

    Add files via upload
    Added code for Figure 12.1

--- Data/RegressionLine_Code.R
+++ Data/RegressionLine_Code.R
@@ -1,8 +1,16 @@
+#Code for Figure 12.0:
 data_minus1 <- ren
 withoutLargestOutlier <- data_minus1[-c(30),]
 withoutLargestOutlier
-withoutLargestOutlier.lm <- lm(X.Gain~Total.Costs, data = withoutLargestOutlier)
+withoutLargestOutlier.lm <- lm(Profit~Total.Costs, data = withoutLargestOutlier)
 coef(withoutLargestOutlier.lm)
-plot(X.Gain~Total.Costs, data = withoutLargestOutlier, main ="Ren's Profit")
+plot(Profit~Total.Costs, data = withoutLargestOutlier, main ="Ren's Profit")
 abline(coef(withoutLargestOutlier.lm))
 
+#Code for Figure 12.1:
+data_minus <- ren
+withoutOuts <- data_minus[-c(1, 11, 8, 30),]
+withoutOuts.lm <- lm(Profit~Total.Costs, data = withoutOuts)
+coef(withoutOuts.lm)
+plot(Profit~Total.Costs, data = withoutOuts, main = "Ren's Profit")
+abline(coef(withoutOuts.lm))
